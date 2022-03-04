2492053db7623ff6a3bdfebb56142c682bc08c55
raghavendram <raghavendram@yupptv.com>
2022-03-04 19:04:29 +0530

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,22 +1,32 @@
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import React from "react";
 import Layout from "../components/layout";
-const Blog = ({data}) => {
+const Blog = ({ data }) => {
   return (
     <Layout title={"Blog"} pageTitle={"My Blog Posts"}>
       <h1>Blog Page</h1>
-      {
-          data.allFile.nodes.map(({name},index)=><li key={index}>{name}</li>)
-      }
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const qeury = graphql`
-  {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+  query MyQuery {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        body
       }
     }
   }
